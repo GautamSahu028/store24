0236c60ae1df4c88e15ecdea509322f6ba16634c
Gautam <gautamsahu19102002@gmail.com>
2025-03-11 18:46:57 +0530

    feat: implement product creation form and update admin route matcher

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher(["/", "/products(.*)", "/about"]);
-const isAdminRoute = createRouteMatcher(["/admin(.*"]);
+const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
 
 export default clerkMiddleware((auth, req) => {
   const isAdminUser = auth().userId === process.env.ADMIN_USER_ID;
